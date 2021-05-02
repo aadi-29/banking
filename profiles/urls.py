c21367cb1b8d7e2ebfa43950c71c76f0667d2cf0
aadi_29 <58077508+aadi_29@users.noreply.github.com>
2021-05-02 23:50:01 +0530

    Project added

---
+++ profiles/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+app_name = "profiles"
+
+urlpatterns = [
+    url(r"^account_status/$", views.index, name = "account_status"),
+    url(r"^money_transfer/", views.money_transfer, name = "money_transfer"),
+    url(r"settings/$", views.settings, name = "settings"),
+    url(r"edit_details/", views.edit_details, name = "edit_details"),
+    url(r"delete_account/$", views.delete_account, name = "delete_account")
+]
